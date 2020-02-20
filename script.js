288316286b7b226a7b90fd23daa0c0e2283e8f89
Sam Gray <sam@sgcreative.net>
2020-02-20 18:50:13 -0500

    Done except for content and contact section

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-$(window).on('load, resize, scroll', function() {
-    $('section').each(function() {
-        var scrollMid = $(window).scrollTop() + ($(window).innerHeight() / 2);
-        var id = $(this).attr('id');
+$(document).ready(function() {
+    $('.percent-bars p').addClass('before-animate');
+});
 
+$(window).on('load, resize, scroll', function() {
+    var scrollMid = $(window).scrollTop() + ($(window).innerHeight() / 2);
+    var scrollLow = $(window).scrollTop() + ($(window).innerHeight() / 1.3);
+
+    $('section').each(function() {
+        var id = $(this).attr('id');
         if (scrollMid > $(this).offset().top) {
             $('nav a').removeClass('active');
             $('nav a[href*=' + id +']').addClass('active');
         }
     });
+
+    if (scrollLow > $('.percent-bars').offset().top) {
+        $('.percent-bars p').removeClass('before-animate');
+    }
+
 });
 
 $('nav a, .hero a').click(function() {
